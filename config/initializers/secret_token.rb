a8f25d5d9f37fb3775d49638a54c570c87c94f35
yurimello <yuri.mcr@gmail.com>
2012-06-20 04:17:39 -0300

    Inicializando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoluntariandoMe::Application.config.secret_token = 'ca934d24a850ff4d59781ecd23f9f54a1f991854e53723e9101a353e5cdbbd05b7e7e530e199741f511ba9a0138847f718060bb13df0020cbf5410c9dcd942f3'
