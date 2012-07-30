430c95419db17a3fa57e3c3a713372fc15e036ed
yurimello <rafael.polo@gmail.com>
2012-07-30 20:26:29 -0300

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name
   # attr_accessible :title, :body
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     logger.info "\n\n\n FACEBOOK: #{auth.info}\n\n\n"
     unless user
       user = User.create(
-                          name: "#{auth.info.first_name} #{auth.info.last_name}",
+                          name: auth.info.email,
                           provider: auth.provider,
                           uid: auth.uid,
                           email: auth.info.email,
