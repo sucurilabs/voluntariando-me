04b98a1df2995ec471d5d302b812724a8a392ac8
yurimello <yuri.mcr@gmail.com>
2012-07-31 03:46:03 -0300

    deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,48 +32,48 @@ set :ssh_options, :forward_agent => true
 set :user, "deployerbot"
 set :keep_releases, 5
 
-set :default_environment, {
-    'PATH' => "/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems/bin:/usr/local/rvm/gems/ruby-1.9.3-p194@global/gems/bin:/usr/local/rvm/bin:$PATH",
-    'RUBY_VERSION' => 'ruby 1.9.3',
-    'GEM_HOME' => '/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems',
-    'GEM_PATH' => '/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems:/usr/local/rvm/gems/ruby-1.9.3-p194@global/gems'
-   # 'BUNDLE_PATH' => '/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems' # If using bundler.
-}
-# set :bundle_gemfile,      "Gemfile"
-#   set :bundle_dir,          fetch(:shared_path)+"/bundle"
-#   set :bundle_flags,        "--deployment --quiet"
-#   set :bundle_without,      [:development, :test]
+# set :default_environment, {
+#     'PATH' => "/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems/bin:/usr/local/rvm/gems/ruby-1.9.3-p194@global/gems/bin:/usr/local/rvm/bin:$PATH",
+#     'RUBY_VERSION' => 'ruby 1.9.3',
+#     'GEM_HOME' => '/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems',
+#     'GEM_PATH' => '/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems:/usr/local/rvm/gems/ruby-1.9.3-p194@global/gems'
+#    # 'BUNDLE_PATH' => '/usr/local/rvm/gems/ruby-1.9.3-p194@voluntariando-me/gems' # If using bundler.
+# }
+# # set :bundle_gemfile,      "Gemfile"
+# #   set :bundle_dir,          fetch(:shared_path)+"/bundle"
+# #   set :bundle_flags,        "--deployment --quiet"
+# #   set :bundle_without,      [:development, :test]
 
 
-namespace :deploy do
-  desc "restarting"
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
+# namespace :deploy do
+#   desc "restarting"
+#   task :restart do
+#     run "touch #{current_path}/tmp/restart.txt"
+#   end
 
-  desc "symlink vendor to shared vendor"
-  task :symlink_vendor_to_shared_vendor do
-    run "ln -s #{current_path}/../shared/vendor #{current_path}/vendor"
-  end
+#   desc "symlink vendor to shared vendor"
+#   task :symlink_vendor_to_shared_vendor do
+#     run "ln -s #{current_path}/../shared/vendor #{current_path}/vendor"
+#   end
 
-  desc "trust rvmrc"
-  task :trust_rvmrc do
-    run "rvm rvmrc trust #{release_path}"
-  end
-end
+#   desc "trust rvmrc"
+#   task :trust_rvmrc do
+#     run "rvm rvmrc trust #{release_path}"
+#   end
+# end
 
-task :ruby_version, :roles => :app do
-  run "ruby -v"
-  run "source /etc/profile; rvm list"
-  run "rvm list"
-  run "rvm gemset list"
-end
+# task :ruby_version, :roles => :app do
+#   run "ruby -v"
+#   run "source /etc/profile; rvm list"
+#   run "rvm list"
+#   run "rvm gemset list"
+# end
 
-task :set_rvm, :roles => :app do
-  run "rvm gemset use voluntariando-me"
-end
+# task :set_rvm, :roles => :app do
+#   run "rvm gemset use voluntariando-me"
+# end
 
-before :deploy, :set_rvm
-before "deploy:assets:precompile", "bundle:install"
-after 'deploy:symlink', 'deploy:symlink_vendor_to_shared_vendor'
-after :deploy, 'deploy:trust_rvmrc'
+# before :deploy, :set_rvm
+# before "deploy:assets:precompile", "bundle:install"
+# after 'deploy:symlink', 'deploy:symlink_vendor_to_shared_vendor'
+# after :deploy, 'deploy:trust_rvmrc'
