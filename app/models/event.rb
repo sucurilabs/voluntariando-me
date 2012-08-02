1d3f1cee0fa056ad1c908fc1d85500eba7bfc999
yurimello <yuri.mcr@gmail.com>
2012-08-02 16:11:23 -0300

    Revert "Adicionando cover ao evento. Issue #10"
    This reverts commit 34da72f6f3557ea976190480e4a4aa7e56b27e14.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,10 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :end_date, :name, :start_date, :owner_id, :cover
+  attr_accessible :description, :end_date, :name, :start_date, :owner_id
 
   belongs_to :owner, :class_name => "User"
   has_many :joins, :class_name => "Event::Join"
   has_many :users, :through => :joins
   
-  mount_uploader :cover, CoverUploader
   def has_user?(user_id)
     self.joins.collect(&:user_id).include?(user_id)
   end  
--- db/migrate/20120802182711_add_cover_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddCoverToEvents < ActiveRecord::Migration
-  def change
-    add_column :events, :cover, :string
-  end
-end
